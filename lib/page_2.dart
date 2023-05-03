5c2c16fa8587472df1bb0ec4c5f0c7d9c2fd3ed7
Emilie Ickart <emi.ickart@gmail.com>
2023-05-03 21:57:40 +0200

    Test - Add stack to adjust CircleAvatar

--- lib/page_2.dart
+++ lib/page_2.dart
@@ -8,15 +8,35 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisSize: MainAxisSize.max, children: [
-      Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
-          child: SizedBox(
-            height: 100,
-            width: double.maxFinite,
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/bitcoin-icon.png'),
-            ),
-          ))
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+      child: SizedBox(
+        height: 100,
+        width: double.maxFinite,
+        child: CircleAvatar(
+          backgroundImage: AssetImage('assets/images/bitcoin-icon.png'),
+        ),
+      )
+    ),
+    Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+      CircleAvatar(
+        radius: 30.0,
+        backgroundImage:
+        NetworkImage("https://via.placeholder.com/150/92c952"),
+          backgroundColor: Colors.transparent,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Image.asset(
+          'assets/images/bitcoin-icon.png',
+          width: 20.9,
+          height: 19.9,
+          ),
+        ),
+      ],
+    )
     ]);
   }
 }
